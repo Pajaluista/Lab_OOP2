d7404098e19f5f3f4d03289ebd2e5211bf93e383
Pajaluista <mt91106@gmail.com>
2025-10-27 18:29:54 +0400

    Delete Lab_OOP3/Lab3 directory

--- Lab_OOP3/Lab3/src/main/java/functions/Insertable.java
+++
@@ -1,8 +0,0 @@
-package functions;
-
-//определяет как вставлять точки в табулированную функцию.
-public interface Insertable {
-
-   //вставляет или обновляет точку (x, y) в табулированной функции
-    void insert(double x, double y);
-}
--- Lab_OOP3/Lab3/src/main/java/functions/Removable.java
+++
@@ -1,4 +0,0 @@
-package functions;
-// Интерфейс для удаления точек из табулированной функции
-// Позволяет удалять точки по указанному индексу
-public interface Removable {void remove(int index);}
